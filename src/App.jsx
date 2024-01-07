50f8876ca043087a73f425c123349484c4e3e74a
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-07 16:39:50 +0330

    feat:develop Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast'
 import routes from './Routes'
 import { useRoutes } from 'react-router-dom'
 import Header from './Components/Header/Header'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   let router = useRoutes(routes)
@@ -11,7 +12,12 @@ function App() {
     <>
      <Toaster />
      <Header />
-      {router}
+     <main className=''>   
+        <div className='grid grid-cols-12 gap-8'>
+         <div className='col-span-2'><Sidebar /></div>
+         <div className='col-span-10'>{router}</div>
+        </div>
+     </main>
     </>
   )
 }
