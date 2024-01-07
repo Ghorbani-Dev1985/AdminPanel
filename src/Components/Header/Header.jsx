761633fda85c43321afb6b1b5d5bda06a2dbddaf
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-07 16:42:26 +0330

    style: change bg in Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import UserImg from '../../assets/Images/github.png'
 function Header() {
   return (
-    <header className='p-4 sticky top-0 z-50 bg-white/10 backdrop-blur-sm border-0 border-b border-b-gray-50 border-solid'>
+    <header className='p-4 sticky top-0 z-50 bg-violet-300/30 backdrop-blur-sm border-0 border-b border-b-violet-300 border-solid'>
       <Box className="container">
         <Box className="flex-between">
          <h1 className='font-MorabbaBold'>پنل کاربری</h1>
