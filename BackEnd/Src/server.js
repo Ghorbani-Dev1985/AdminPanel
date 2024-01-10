0ca64a706d5843ed4474f3323e3d96fc84800f62
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-10 14:03:50 +0330

    feat: complete LastTransactions component with real Backend

--- BackEnd/Src/server.js
+++ BackEnd/Src/server.js
@@ -7,74 +7,16 @@ const AdminModel = require('./Models/Admin')
 
 const userRouter = require('./Routes/userRouter')
 const xAxiosDataRouter = require('./Routes/xAxiosDataRouter')
+const transactionRouter = require('./Routes/transactionRouter')
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/api/users' , userRouter)
 app.use('/api/xAxiosData' , xAxiosDataRouter)
+app.use('/api/transaction' , transactionRouter)
 
 mongoose.connect('mongodb://127.0.0.1:27017/AdminPanel');
 mongoose.Promise = global.Promise;
-
-// let mohsenInfo = {
-//     firstName: 'محمد',
-//     lastName: 'قربانی',
-//     phoneNumber: '09358923824',
-//     userName: 'moh1985ghDev',
-//     password: 'moh1985ghDev'
-// }
-
-// let phoneCategory = new CategoryModel({
-//   title: 'سامسونگ'
-// })
-
-// let mohUser = new UserModel(mohsenInfo)
-
-// mohUser.save().then(() => {
-//     console.log('New user inserted')
-// })
-
-// phoneCategory.save().then(() => {
-//   console.log('New category inserted')
-// })
-
-// UserModel.findOneAndDelete({firstName: 'lpln'}).then(deleteUser => {
-//   console.log('One user deleted')
-// })
-
-// UserModel.findByIdAndDelete('659a5e96693bfae019d45b2b').then(deleteUser => {
-//   console.log('One user delete by id')
-// })
-
-// UserModel.find({ firstName: 'محسن' }).then(userFind => {
-//   console.log(userFind)
-// })
-
-// UserModel.findById('659a5e9725dc93e52cd8d77a').then(userFindByID => {
-//   console.log('userFindByID' ,userFindByID)
-// })
-
-// UserModel.find({}).then(getAll => {
-//   console.log(getAll)
-// })
-
-// UserModel.findOneAndUpdate({firstName : 'محسن'} , {
-//   firstName: 'محیا',
-//   lastName: 'پرویزی',
-//   userName: 'mahyaParvizi123',
-//   password: 'mahyaParvizi1232112393'
-// }).then(userUpdate => {
-//   console.log('User Updated....')
-// })
-
-// UserModel.findByIdAndUpdate('659bfb012c84cf4a400fd6d9' , {
-//   firstName: 'نیلوفر',
-//   lastName: 'مرادی',
-//   userName: 'niloMoradi434566',
-//   password: 'kolifdj123445fe',
-// }).then(result => {
-//   console.log('By ID Updated' , result)
-// })
 
 
 
--- src/Components/LastTransactions/LastTransactions.jsx
+++ src/Components/LastTransactions/LastTransactions.jsx
@@ -1,71 +1,70 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import { BaseURL } from "../../Utils/Utils";
+import Toman from '../../assets/Images/svgs/toman-black.svg'
 
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-function createData(customer, date, amout, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
 function lastTransactions() {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${BaseURL}transaction/all`)
+      .then((response) => setTransactions(response.data));
+  }, []);
   return (
     <Box className="flex flex-col flex-[2] my-8 shadow-round p-2 rounded-lg">
-       <h3 className='my-4'> تراکنش های اخیر</h3>
-       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <h3 className="my-4"> تراکنش های اخیر</h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">نام و نام خانوادگی</TableCell>
+              <TableCell align="center">تاریخ</TableCell>
+              <TableCell align="center">مبلغ</TableCell>
+              <TableCell align="center">وضعیت</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactions.slice(0 , 6).map(
+              ({ id, firstName, lastName, createdAt, price, status }) => (
+                <StyledTableRow key={id}>
+                  <TableCell component="th" scope="row" align="center" className="flex-center gap-1">
+                    {firstName} {lastName}
+                  </TableCell>
+                  <TableCell align="center">{createdAt}</TableCell>
+                  <TableCell align="center" className="flex-center gap-1 font-DanaBold">{price.toLocaleString()}
+                  <img src={Toman} alt="ghorbani-dev.ir" className="size-5" />
+                  </TableCell>
+                  <TableCell align="center"><span className={`${status === "تکمیل شده" ? "bg-emerald-100 text-emerald-400" : status === "پرداخت نشده" ? "bg-amber-100 text-amber-400" : status === "پرداخت ناموفق" ? "bg-rose-100 text-rose-400" : "bg-gray-100 text-gray-400"} px-1 py-1 rounded-lg`}>{status}</span></TableCell>
+                  
+                </StyledTableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
-  )
+  );
 }
 
-export default lastTransactions
+export default lastTransactions;
