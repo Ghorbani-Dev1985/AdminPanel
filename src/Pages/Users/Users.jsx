48409575927450217909beaff924819482afe571
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-10 21:08:59 +0330

    lang: add fa lang to datagrid

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Box } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid ,faIR} from '@mui/x-data-grid';
 import axios from 'axios';
 import { BaseURL } from '../../Utils/Utils';
 import UsersSkeleton from '../../Components/common/UsersSkeleton/UserSkeleton'
@@ -8,27 +8,27 @@ const columns = [
   {
     field: 'id',
     headerName: 'ردیف',
-    with: 40,
+    width: 60,
   },
   {
     field: 'firstName',
     headerName: 'نام',
-    with: 100,
+    width: 100,
   },
   {
     field: 'lastName',
     headerName: 'نام خانوادگی',
-    with: 150,
+    width: 150,
   },
   {
     field: 'userName',
     headerName: 'نام کاربری',
-    with: 190,
+    width: 190,
   },
   {
     field: 'title',
     headerName: 'عنوان تخصص ',
-    with: 300,
+    width: 300,
   }
 ]
 
@@ -39,24 +39,19 @@ function Users() {
   .get(`${BaseURL}users/all`)
   .then(response => setUsers(response.data))
   } , []);
-  console.log(users)
 
   return (
     <Box>
       <h2 className='font-MorabbaBold mb-8'>لیست کاربران</h2>
      {
-      users.length ? <DataGrid rows={users.map((user,index)=>{return {id:index+1,...user}})}  localeText={{
-        MuiTablePagination: {
-          labelDisplayedRows: ({ from, to, count }) =>
-            `${from} - ${to} از ${count} کاربر`,
-        },
-      }} getRowId={(row) => row._id} columns={columns}  initialState={{
+      users.length ? <DataGrid rows={users.map((user,index)=>{return {id:index+1,...user}})} localeText={faIR.components.MuiDataGrid.defaultProps.localeText}
+       getRowId={(row) => row._id} columns={columns}  initialState={{
         pagination: {
           paginationModel: {
             pageSize: 5,
           },
         },
-      }} disableRowSelectionOnClick pageSizeOptions={5}/> :  <UsersSkeleton listsToRender={4}/>
+      }} disableRowSelectionOnClick pageSizeOptions={5} /> :  <UsersSkeleton listsToRender={4}/>
      }
     </Box>
   )
