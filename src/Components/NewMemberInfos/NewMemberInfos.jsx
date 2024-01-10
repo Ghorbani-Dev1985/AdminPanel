cedc20d0f9d533f8f7481e217fe19f8d754bbe2f
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-10 18:32:20 +0330

    feat: add skeleton to NewMemberInfos component

--- src/Components/NewMemberInfos/NewMemberInfos.jsx
+++ src/Components/NewMemberInfos/NewMemberInfos.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {BaseURL} from '../../Utils/Utils'
-import { Box } from '@mui/material'
+import { Avatar, Box, Skeleton } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import axios from 'axios';
@@ -8,15 +8,18 @@ import axios from 'axios';
 
 function NewMemberInfos() {
     const [users , setUsers] = useState([])
+    let showLoading = 4
     useEffect(() => {
     axios
     .get(`${BaseURL}users/all`)
     .then(response => setUsers(response.data))
     } , [users]);
+    
   return (
     <Box className="flex flex-col flex-1 my-8 shadow-round p-2 rounded-lg">
       <h3 className='my-4'>کاربران جدید</h3>
       {
+        users.length ?
         users.slice(0 , 4).map(({id, userName ,title}) => {
             return(
                 <div key={id} className='flex-between bg-violet-100/0 border border-gray-100 border-solid rounded-lg p-2 my-2'>
@@ -29,9 +32,33 @@ function NewMemberInfos() {
                 </div>
             )
         })
+        :  <ListSkeleton listsToRender={4}/>
       }
     </Box>
   )
 }
 
 export default NewMemberInfos
+
+
+const ListSkeleton = ({listsToRender}) => {
+  return (
+    <>
+    {
+      Array(listsToRender)
+      .fill(1)
+      .map((card, index) => (
+        <React.Fragment key={index}> 
+          <div className='w-full flex-center gap-1 my-4'>
+          <Skeleton variant="circular">
+          <Avatar />
+        </Skeleton> <Skeleton animation="wave" className='w-full h-12'/>
+               </div>
+          </React.Fragment>
+        ))
+      }
+      </>
+  );
+};
+
+export {ListSkeleton}
--- src/Components/common/Chart/Chart.jsx
+++ src/Components/common/Chart/Chart.jsx
@@ -1,13 +1,15 @@
-import { Box } from '@mui/material'
+import { Box, Skeleton } from '@mui/material'
 import React from 'react'
 import {ResponsiveContainer , LineChart , Line , XAxis , CartesianGrid, Tooltip} from 'recharts'
 
 
 function Chart({key, title , data , dataKey , grid}) {
+  console.log(data)
   return (
     <Box key={key} className="my-8 shadow-round p-2 rounded-lg">
       <h3 className='my-3'>{title}</h3>
-      <ResponsiveContainer width="100%" aspect={4}>
+      {
+        data.length ? <ResponsiveContainer width="100%" aspect={4}>
         <LineChart data={data} >
             <XAxis dataKey="name" stroke='#5550bd'/>
             <Line type="monotone" dataKey={dataKey} stroke='#5550bd'/>
@@ -16,7 +18,8 @@ function Chart({key, title , data , dataKey , grid}) {
                 grid && <CartesianGrid stroke='#c0dfdf' strokeDasharray="10" />
             }
         </LineChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer>   : <Skeleton variant="rounded" className='w-full h-full min-h-96' />
+      }
     </Box>
   )
 }
